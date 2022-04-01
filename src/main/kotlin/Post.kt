39f69a834cd2b587f437c3cbe8205a775b2a80fd
maks-x <100487620+maks-x@users.noreply.github.com>
2022-04-01 21:42:58 +0300

    Added nullable post prorerties(id, text, postSource, geo, copyHistory)

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -4,12 +4,12 @@ import java.time.LocalDateTime
 import java.time.ZoneOffset
 
 data class Post(
-    val id: Int = 0,
+    val id: Int? = null,
     val ownerID: Int = 1,
     val fromID: Int = 1,
     val createdBy: Int = 0,
     val date: Int = (System.currentTimeMillis() / 1000).toInt(),
-    val text: String,
+    val text: String? = null,
     val replyOwnerID: Int = 0,
     val replyPostID: Int = 0,
     val friendsOnly: Boolean = false,
@@ -19,7 +19,10 @@ data class Post(
     val reposts: Reposts = Reposts(),
     val views: Views = Views(),
     val postType: String = "post",
+    val postSource: PostSource? = null,
+    val geo: Geo? = null,
     val signerID: Int = 0,
+    val copyHistory: Array<Post>? = null,
     val canPin: Boolean = false,
     val canDelete: Boolean = false,
     val canEdit: Boolean = false,
@@ -43,6 +46,26 @@ data class Post(
             ***
             $views  $likes  $comments   $reposts
         """.trimIndent()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Post
+
+        if (id != other.id) return false
+        if (fromID != other.fromID) return false
+        if (date != other.date) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id ?: 0
+        result = 31 * result + fromID
+        result = 31 * result + date
+        return result
     }
 
 }
